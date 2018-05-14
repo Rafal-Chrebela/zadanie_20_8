ab355893da16ed54ebfb4157d90885de2c9abfc6
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-05-15 00:12:44 +0200

    Add database

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/nodeappdatabase", {
-  useMongoClient: true
-});
+mongoose.connect(
+  "mongodb://<Rafal>:<Trzyka_888>@ds119160.mlab.com:19160/rasfael-1",
+  {
+    useMongoClient: true
+  }
+);
 
 //new user Schema
 const userSchema = new Schema({
